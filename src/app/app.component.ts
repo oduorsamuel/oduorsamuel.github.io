d13cff794820e5854682d405d8050f6c4a407e45
soduor1 <soduor1@safaricom.co.ke>
2025-01-06 17:33:57 +0300

    Revamp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,41 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [PdfViewerModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
-
 export class AppComponent {
-  state = true;
-  state2 = false;
-  link_fn() {
-     this.state = true;
-     this.state2 = false;
+  title = 'John Doe - Resume';
+  pdfSrc = "a.pdf";
+  currentPage: number = 1;
+  totalPages: number = 0;
+  
+  // PDF viewer options
+  pdfOptions = {
+    renderText: true,
+    originalSize: false,
+    showAll: false,  // Disable showing all pages
+  };
+
+  afterLoadComplete(pdfData: any) {
+    this.totalPages = pdfData.numPages;
   }
 
-  link_fn2() {
-    this.state2 = !this.state2;
-    this.state = false;
- }
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
 }
